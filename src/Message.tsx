8ee30a985238f44b07f0df8228a4351bf774652a
Stephanie Smithson <stephaniesmithson16@gmail.com>
2023-09-19 20:57:41 -0500

    Initial commit

---
+++ src/Message.tsx
@@ -0,0 +1,10 @@
+// PascalCase for TypeScript
+function Message() {
+    // JSX: JavaScript XML
+    const name = 'Stephanie';
+    if(name)
+        return <h1>Hello {name}</h1>;
+    return <h1>Hello World</h1>
+}
+
+export default Message;
